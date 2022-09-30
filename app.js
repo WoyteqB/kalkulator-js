667e91f89f6ea2ebde775bb798819cec8924bb92
wojtek.brzoska@gmail.com <grafik@up.local>
2022-09-30 15:52:43 +0200

    dodanie potegowania i pierwiastkowania

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const buttons = document.querySelectorAll(".kalkulator__panel-bth button");
 //console.log(numberButtons);
 
 let firstNumber = 0;
-let secondNumber = 0;
 let tmpNumber = "";
 let lastAction = "";
 let action = "";
@@ -22,8 +21,7 @@ let setDisplaySecondLine = (value) => {
 
 buttons.forEach( button => button.addEventListener("click", (e)=> buttonClick(e.target.innerHTML, e.target.className)));
 const buttonClick = (value, className) => {
-    
-    if(className === "number"){
+    if(className.includes("number")){
         
         if(lastAction === "="){
             setDisplayFirstLine("");
@@ -33,7 +31,7 @@ const buttonClick = (value, className) => {
             console.log("liczba po =");
         }
 
-        if(value === "," && tmpNumber.includes(",")){
+        if(value === "," && tmpNumber.includes(".")){
             console.log("Error - już jest jeden przecinek w liczbie");
         }else if(value === "0" && tmpNumber ==="0"){
             console.log("Error - jedno zero na przodzie wystarczy");
@@ -48,19 +46,63 @@ const buttonClick = (value, className) => {
         }else if( value === "+/-"){
             tmpNumber = (parseInt(tmpNumber) * -1).toString();
             setDisplaySecondLine(tmpNumber);
+        }else if( value === ","){
+            tmpNumber = tmpNumber + ".";
+            setDisplaySecondLine(tmpNumber);
         }else{
             tmpNumber ==="0" && value!=="0" ? tmpNumber = value : tmpNumber = tmpNumber + value;
             setDisplaySecondLine(tmpNumber);
         }
-    }else if(className === "action"){
+    }else if(className.includes("action")){
         console.log("Akcja (L1:"+firstNumber+", L2: "+tmpNumber+", Last action: "+lastAction+", Action: "+ value +")")
-        if(lastAction === ""){
+        if(value == "C"){
+            console.log("action C");
+            setDisplayFirstLine("");
+            setDisplaySecondLine(0);
+            tmpNumber="0";
+            firstNumber = 0;
+            lastAction = "";
+            action=""
+        }else if( value === "√"){
+            if(lastAction === ""){
+                setDisplayFirstLine("√("+tmpNumber+")");
+                tmpNumber = Math.sqrt(parseInt(tmpNumber));
+                setDisplaySecondLine(tmpNumber);
+            }else{
+                setDisplayFirstLine(firstNumber +" "+lastAction+" √("+tmpNumber+")");
+                tmpNumber = Math.sqrt(parseInt(tmpNumber));
+                setDisplaySecondLine(tmpNumber);
+            }
+
+        }else if( value === "x<sup>2</sup>"){
+            if(lastAction === ""){
+                setDisplayFirstLine("sqr("+tmpNumber+")");
+                tmpNumber = Math.pow(parseInt(tmpNumber),2);
+                setDisplaySecondLine(tmpNumber);
+            }else{
+                setDisplayFirstLine(firstNumber +" "+lastAction+" "+tmpNumber+"<sup>2</sup>");
+                tmpNumber = Math.pow(parseInt(tmpNumber),2);
+                setDisplaySecondLine(tmpNumber);
+            }
+        }else if( value === "<sup>1</sup>/<sub>x</sub>"){
+            if(lastAction === ""){
+                setDisplayFirstLine("1/"+tmpNumber+")");
+                tmpNumber = 1/parseInt(tmpNumber);
+                setDisplaySecondLine(tmpNumber);
+            }else{
+                setDisplayFirstLine(firstNumber +" "+lastAction+" 1/"+tmpNumber);
+                tmpNumber = 1/parseInt(tmpNumber);
+                setDisplaySecondLine(tmpNumber);
+            }
+        }else if(lastAction === ""){
+            console.log("last action empty");
             firstNumber = parseInt(tmpNumber);
             setDisplayFirstLine(tmpNumber+" "+value);
             setDisplaySecondLine(0);
             tmpNumber="0";
             lastAction = value;
         }else if(lastAction === "="){
+            console.log("last action =");
             setDisplayFirstLine(firstNumber+" "+value);
             setDisplaySecondLine(0);
             tmpNumber="0";
@@ -68,28 +110,30 @@ const buttonClick = (value, className) => {
         }else{
             switch(value){
                 case "=":
+                    console.log("action =");
                     setDisplayFirstLine(firstNumber +" "+ lastAction +" "+ tmpNumber +" =");
                     firstNumber = makeAction(firstNumber, parseInt(tmpNumber), lastAction);
                     setDisplaySecondLine(firstNumber);
                     lastAction="=";
-                    brake;
+                    break;
                 default: 
-                    
+                    console.log("action def "+ value);
                     if(tmpNumber === 0 && lastAction === "/"){
                         setDisplayFirstLine("");
                         setDisplaySecondLine("Nie można dzielic przez 0");
                         firstNumber=0;
                         lastAction="";
-                        break;
+                        
                     }else{     
                         firstNumber = makeAction(firstNumber, parseInt(tmpNumber), lastAction);
                         setDisplayFirstLine(firstNumber +" "+ value);
                         setDisplaySecondLine("0");
                         tmpNumber = "0"
-                    }           
+                        lastAction = value;
+                    }
+                    break;    
                     
             }
-            lastAction = value;
         }
 
         
@@ -100,7 +144,7 @@ const buttonClick = (value, className) => {
 let makeAction = (value1, value2, action) => {
     switch(action){
         case "+":
-            return value1 + value2;
+            return value1+value2;
             break;
         case "-":
             return value1 - value2;
